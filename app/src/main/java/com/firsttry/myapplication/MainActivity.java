c1b2e7b311c9294a107ae1fc7815112a38aab86c
Bishalsharma733 <bishalsharma733@gmail.com>
2021-05-17 13:12:38 +0545

    Few changes

--- app/src/main/java/com/firsttry/myapplication/MainActivity.java
+++ app/src/main/java/com/firsttry/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.firsttry.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,7 +24,22 @@ public class MainActivity extends AppCompatActivity {
         output = findViewById(R.id.output);
         button = findViewById(R.id.button);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+
+                String faren = input.getText().toString();
+                double fahrenheit = Double.parseDouble(faren);
+                double celsius = (fahrenheit-32)*5.0/9.0;
+                output.setText("The temperature in Celsius is " + celsius);
+
+
+
+
+
+            }
+        });
 
     }
 }
