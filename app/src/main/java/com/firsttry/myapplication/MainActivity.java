fc5c0dc786d89046e8eed0187b7c9eaedbd8daca
Bishalsharma733 <bishalsharma733@gmail.com>
2021-05-17 12:33:45 +0545

    first commit

---
+++ app/src/main/java/com/firsttry/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.firsttry.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText input;
+    TextView output;
+    Button button;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        input = findViewById(R.id.input);
+        output = findViewById(R.id.output);
+        button = findViewById(R.id.button);
+
+
+
+    }
+}
